4121ea6270e76e44405e7140ab8b87ef39df7bbf
Uchka08 <enkhuchralenkhuchral@gmail.com>
2023-01-19 22:32:37 -0800

    added dominant.py

---
+++ dominant.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2 as cv
+
+
+cap = cv.VideoCapture(0)
+n_clusters = 5
+
+while True:
+    status, image = cap.read()
+    if not status:
+        break
+
+    # to reduce complexity resize the image
+    data = cv.resize(image, (100, 100)).reshape(-1, 3)
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    flags = cv.KMEANS_RANDOM_CENTERS
+    compactness, labels, centers = cv.kmeans(data.astype(np.float32), n_clusters, None, criteria, 10, flags)
+
+    cluster_sizes = np.bincount(labels.flatten())
+
+    palette = []
+    for cluster_idx in np.argsort(-cluster_sizes):
+        palette.append(np.full((image.shape[0], image.shape[1], 3), fill_value=centers[cluster_idx].astype(int), dtype=np.uint8))
+    palette = np.hstack(palette)
+
+    sf = image.shape[1] / palette.shape[1]
+    out = np.vstack([image, cv.resize(palette, (0, 0), fx=sf, fy=sf)])
+
+    cv.imshow("dominant_colors", out)
+    cv.waitKey(1)
