48a1670f06362530365dbb1bda64eeffc3241b19
Uchka08 <enkhuchralenkhuchral@gmail.com>
2023-03-14 12:25:01 -0700

    added speech_rec

--- Lab2/sp_rec.py
+++ Lab2/sp_rec.py
@@ -1,6 +1,28 @@
+
+#!/usr/bin/env python3
+# Requires PyAudio and PySpeech.
+
 import speech_recognition as sr
-r = sr.Recognizer()
-with sr.Microphone() as source:
-   print("Say something!")
-   audio = r.listen(source)
-   print(r.recognize_google(audio))
+def speech_rec():
+   # Record Audio
+   r = sr.Recognizer()
+   result = 0
+   with sr.Microphone() as source:
+      print("Say something!")
+      audio = r.listen(source, 10, 3)
+   # Speech recognition using Google Speech Recognition
+   try:
+      # for testing purposes, we're just using the default API key
+      # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+      # instead of `r.recognize_google(audio)`
+      result = format(r.recognize_google(audio))
+   except sr.UnknownValueError:
+      result = 0
+   except sr.RequestError:
+      result = 0
+   if result == 'switch' or result =='search' or result =='which' or result =='sweet':
+      return 'switch'
+   else:
+      return 0
+
+print(speech_rec())
